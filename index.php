9bfd4db9e2731304980f2f34b04a4e880904f0ba
Arturo lara <al221110311@gmail.com>
2014-10-24 02:17:27 -0500

    MVC
    modo

---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<?php
+/**
+ * Created by PhpStorm.
+ * User: ÄrturÖ
+ * Date: 16/10/2014
+ * Time: 05:41 PM
+ */
+
+require('template/header.php');
+require('clases/Usuario.php');
+require('helpers.php');
+require('conexion.php');
+
+if (empty($_GET['url']))
+    $_GET['url']='home';
+
+controller($_GET['url']);
+
+
+require('template/footer.php');
+
+?>
---
+++ template/menu.php
@@ -0,0 +1,70 @@
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a  class="navbar-brand active" href="#"><strong>Modelo Vista Controlador </strong></a>
+        </div>
+
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+		  <li><a href="home"><i class="glyphicon glyphicon-home"> Home</i></a></li>
+              <li><a href="nosotros"><i class="glyphicon glyphicon-eye-open"> Nosotros</i></a></li>
+			<li><a href="galeria"><i class="glyphicon glyphicon-camera"> Galeria</i></a></li>
+            <li><a href="contacto"><i class="glyphicon glyphicon-comment"> Contacto</i></a></li>
+            <li><a href="myusuario"><i class="glyphicon glyphicon-user"> Usuario</i></a></li>
+
+
+           
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">MORE <span class="caret"></span></a>
+              <ul class="dropdown-menu" role="menu">
+                <li><a href="cerrar.php"><i class="glyphicon glyphicon-eject"> Salir</i></a></li>
+
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                  <li class="divider"></li>
+                <li><a href="#">Programmer more</a></li>
+                
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+
+            <li class="active"><a href="#">Control escolar</a></li>
+            
+
+            
+
+
+
+    </ul>
+</div><!--/.nav-collapse -->
